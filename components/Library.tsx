9f89861db23d03e2aa442c52df747dc0642d6054
Dawitlior <dawitlior777@gmail.com>
2023-07-12 06:07:27 +0300

    layout+auth+supabase

---
+++ components/Library.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai"
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+
+const Library = () => {
+    const authModal = useAuthModal();
+    const { user, subscription } = useUser();
+    const uploadModal = useUploadModal();
+    const onClick = () => {
+        if (!user) {
+            return authModal.onOpen();
+        }
+
+        return uploadModal.onOpen();
+    }
+
+    return (
+        <div className="flex flex-col">
+            <div className="flex items-center justify-between px-5 pt-4">
+                <div className="inline-flex items-center gap-x-2">
+                    <TbPlaylist className="text-neutral-400" size={30} />
+                    <p className="text-neutral-400 font-medium text-md">
+                        Your library
+                    </p>
+                </div>
+                <AiOutlinePlus
+                onClick={onClick}
+                size={26}
+                className="cursor-pointer text-neutral-400 hover:text-white transition"
+                />
+            </div>
+            <div className="flex flex-col gap-y-2 mt-4 px-3 ">
+                list of songs
+            </div>
+        </div>
+    )
+}
+
+export default Library
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import AuthModal from "@/components/AuthModal";
+import Modal from "@/components/Modal";
+import UploadModal from "@/components/UploadModal";
+
+import { useEffect, useState } from "react";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <AuthModal />
+      <UploadModal />
+    </>
+  );
+};
+
+export default ModalProvider;
