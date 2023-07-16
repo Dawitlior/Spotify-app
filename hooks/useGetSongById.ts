e8411200f3ab719f8c2e4897c65f0835b63b2c26
Dawitlior <dawitlior777@gmail.com>
2023-07-16 19:13:05 +0300

    fix to camelCase

--- hooks/useGetSongById.ts
+++ hooks/useGetSongById.ts
@@ -4,7 +4,7 @@ import { useEffect, useMemo, useState } from "react"
 import { toast } from "react-hot-toast";
 
 
-const useGetSongByid = (id?: string) => {
+const useGetSongById = (id?: string) => {
     const [isLoading, setIsLoading] = useState(false);
     const [song, setSong] = useState<Song | undefined>(undefined)
     const { supabaseClient } = useSessionContext();
@@ -41,4 +41,4 @@ const useGetSongByid = (id?: string) => {
     }), [isLoading, song]) 
 }
 
-export default useGetSongByid
+export default useGetSongById
