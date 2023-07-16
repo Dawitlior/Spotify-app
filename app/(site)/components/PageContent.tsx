1cf74b5e222eabdbbb1db21af8b923ace332ea93
Dawitlior <dawitlior777@gmail.com>
2023-07-16 19:14:11 +0300

    use hook useOnPlay in pageContent compo

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types"
 
 interface PageContentProps {
@@ -10,6 +11,7 @@ interface PageContentProps {
 const PageContent: React.FC<PageContentProps> = ({
     songs
 }) => {
+    const onPlay = useOnPlay(songs);
     if (songs.length === 0) {
         return (
             <div className="mt-4 text-neutral-400">
@@ -22,7 +24,7 @@ const PageContent: React.FC<PageContentProps> = ({
             {songs.map((item) => (
                 <SongItem
                     key={item.id}
-                    onClick={() =>{}}
+                    onClick={(id: string) => onPlay(id)}
                     data={item}
                 />
             ))}
