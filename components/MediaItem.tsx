11ff8d9e0ff2162198f4ee065b301537f5a3a8bb
Dawitlior <dawitlior777@gmail.com>
2023-07-17 22:18:44 +0300

    add and return player.setId

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -7,29 +7,26 @@ import { Song } from "@/types";
 import usePlayer from "@/hooks/usePlayer";
 
 interface MediaItemProps {
-    data: Song;
-    onClick?: (id: string) => void;
+  data: Song;
+  onClick?: (id: string) => void;
 }
 
-const MediaItem: React.FC<MediaItemProps> = ({
-    data,
-    onClick,
-}) => {
-    const player = usePlayer();
-    const imageUrl = useLoadImage(data);
+const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+  const player = usePlayer();
+  const imageUrl = useLoadImage(data);
 
-    const handleClick = () => {
-        if (onClick) {
-            return onClick(data.id);
-        }
+  const handleClick = () => {
+    if (onClick) {
+      return onClick(data.id);
+    }
 
-        return player.setId(data.id);
-    };
+    return player.setId(data.id);
+  };
 
-    return (
-        <div
-            onClick={handleClick}
-            className="
+  return (
+    <div
+      onClick={handleClick}
+      className="
         flex 
         items-center 
         gap-x-3 
@@ -39,31 +36,29 @@ const MediaItem: React.FC<MediaItemProps> = ({
         p-2 
         rounded-md
       "
-        >
-            <div
-                className="
+    >
+      <div
+        className="
           relative 
           rounded-md 
           min-h-[48px] 
           min-w-[48px] 
           overflow-hidden
         "
-            >
-                <Image
-                    fill
-                    src={imageUrl || "/images/music-placeholder.png"}
-                    alt="MediaItem"
-                    className="object-cover"
-                />
-            </div>
-            <div className="flex flex-col gap-y-1 overflow-hidden">
-                <p className="text-white truncate">{data.title}</p>
-                <p className="text-neutral-400 text-sm truncate">
-                    By {data.author}
-                </p>
-            </div>
-        </div>
-    );
-}
+      >
+        <Image
+          fill
+          src={imageUrl || "/images/music-placeholder.png"}
+          alt="MediaItem"
+          className="object-cover"
+        />
+      </div>
+      <div className="flex flex-col gap-y-1 overflow-hidden">
+        <p className="text-white truncate">{data.title}</p>
+        <p className="text-neutral-400 text-sm truncate">By {data.author}</p>
+      </div>
+    </div>
+  );
+};
 
 export default MediaItem;
