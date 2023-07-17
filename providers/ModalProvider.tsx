0c7efa32145be1603a2e34201461b088e7a87ec5
Dawitlior <dawitlior777@gmail.com>
2023-07-17 22:15:12 +0300

    add interface

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -2,11 +2,17 @@
 
 import AuthModal from "@/components/AuthModal";
 import Modal from "@/components/Modal";
+import SubscribeModal from "@/components/SubscribeModal";
 import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
 
 import { useEffect, useState } from "react";
 
-const ModalProvider = () => {
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({products}) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -21,6 +27,7 @@ const ModalProvider = () => {
     <>
       <AuthModal />
       <UploadModal />
+      <SubscribeModal products={products} />
     </>
   );
 };
